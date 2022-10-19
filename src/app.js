f5f2253361b5079ee74b9a926e65f831aa949c44
LOGEDI <nelsonlogedi@gmail.com>
2022-10-19 16:15:28 +0000

    my post card final

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let domainNames = [];
+  let adj = ["beautiful", "scariest", "Furry"];
+  let noun = ["mountain", "teacher", "woman"];
+  let pronoun = ["yours", "where", "he", "nobody"];
+  let domain = [".com", ".net", ".us", ".fr", ".es", ".ch", ".co.uk"];
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let k = 0; k < adj.length; k++) {
+      for (let j = 0; j < noun.length; j++) {
+        for (let p = 0; p < domain.length; p++) {
+          domainNames.push(pronoun[i] + adj[k] + noun[j] + domain[p]);
+        }
+      }
+    }
+  }
+  console.log(domainNames);
+
+  let outputDomains = "";
+
+  for (let domain of domainNames) {
+    outputDomains += `<li>${domain}</li>`;
+  }
+  document.querySelector("#domain-list").innerHTML = outputDomains;
 };
